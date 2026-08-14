47f1b5e49e9ef5f712019245b18763bb274fa792
Fahad Ahmed <fahadmemon3299@gmail.com>
2026-08-14 21:11:01 +0500

    feat: make axios error readable

--- apps/backend/src/controllers/book.controller.ts
+++ apps/backend/src/controllers/book.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { fetchBooks } from "../services/book.service"
 import logger from "../config/logger"
+import axios from "axios"
 
 export const searchBooks = async (req: Request, res: Response) => {
     try {
@@ -9,7 +10,15 @@ export const searchBooks = async (req: Request, res: Response) => {
         const result = await fetchBooks(query as string)
         res.json(result)
     } catch (error) {
-        logger.error(`Error searching for books: ${error}`);
+        if (axios.isAxiosError(error)) {
+            logger.error(
+                `Google Books error ${error.response?.status}: ${JSON.stringify(
+                    error.response?.data
+                )}`
+            );
+        } else {
+            logger.error(`Unexpected error: ${error}`);
+        }
         res.status(500).json({ message: 'Internal server error' })
     }
 }
